4474ccb9102f1b5a01905b5f02c6a4d6f96a64a0
Keith Tom <keith.tom@gmail.com>
2011-12-16 16:02:32 -0800

    fix

--- lib/resources/purchase.rb
+++ lib/resources/purchase.rb
@@ -8,8 +8,8 @@ module OfferEngine
       :slug => "34063ec2",
       :purchase_id => "34063ec2",
       :deal_type => "standard",
-      :deal_slug => OfferEngine::SHORT_DEAL[:id],
-      :deal_id => OfferEngine::SHORT_DEAL[:id],
+      :deal_slug => OfferEngine.short_deal[:id],
+      :deal_id => OfferEngine.short_deal[:id],
       :price => 1000,
       :amount => 950,
       :number_bought => 1,
@@ -18,7 +18,7 @@ module OfferEngine
       :created_at => "Fri Dec 16 19:11:15 UTC 2011",
       :user_id => "fce3237b",
       :credit_card_id => "a877875fc3",
-      :deal => OfferEngine::SHORT_DEAL,
+      :deal => OfferEngine.short_deal,
       :coupons => [OfferEngine.coupon],
     }.merge(opts)
   end
